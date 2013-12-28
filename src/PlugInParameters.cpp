95a1b87cdf92d87e162b28b6af6bf52db707f342
Ion Parau <iparau@mikogo.com>
2013-12-28 13:15:38 +0200

    Initial Commit
    Signed-off-by: Ion Parau <iparau@mikogo.com>

---
+++ src/PlugInParameters.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+
+#include <cppunit/plugin/PlugInParameters.h>
+
+#if !defined(CPPUNIT_NO_TESTPLUGIN)
+
+CPPUNIT_NS_BEGIN
+
+
+PlugInParameters::PlugInParameters( const std::string &commandLine )
+    : m_commandLine( commandLine )
+{
+}
+
+
+PlugInParameters::~PlugInParameters()
+{
+}
+
+
+std::string 
+PlugInParameters::getCommandLine() const
+{
+  return m_commandLine;
+}
+
+
+CPPUNIT_NS_END
+
+#endif // !defined(CPPUNIT_NO_TESTPLUGIN)
